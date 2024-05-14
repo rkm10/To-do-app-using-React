3ab256cc5bf5bf8b5d39cf9e2583d9488c27c946
rkm10 <rajkumarmalluri08@gmail.com>
2024-05-14 20:44:21 +0530

    to-do patch

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [todoList, updateTodo]
+    = useState(
+      [
+        {
+          id: 1,
+          task: 'Learn React'
+        },
+
+        {
+          id: 2,
+          task: 'Master React in 10 days'
+        }
+      ]
+    )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+
+    <div className="container mt-5 w-50">
+      <h3 className='text-center'>To-Do App using React</h3>
+      <div className="input-group">
+        <input className="form-control" type="text" />
+        <button button className="btn btn-primary">Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <ul className="list-group mt-4">
+        <li className="list-group-item">
+          <p>todo 1</p>
+          <button>❌</button>
+        </li>
+        <li className="list-group-item">
+          <p>todo 2</p>
+          <button>❌</button>
+        </li>
+      </ul>
+    </div>
+
+
   )
 }
 
