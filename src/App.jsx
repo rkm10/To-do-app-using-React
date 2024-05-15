4e2c0388050a9bc3e6ecb7bfb0e338320450e2a2
rkm10 <rajkumarmalluri08@gmail.com>
2024-05-15 15:34:52 +0530

    To-do app optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,77 @@
-import './App.css'
-import React, { useState } from 'react'
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+      let [todoinput, updateinput] = useState("");
 
-  let [todoList, updateTodo]
-    = useState(
-      [
-        {
-          id: 1,
-          task: 'Learn React'
-        },
+      let [todoList, updateTodo] = useState([
+            {
+                  id: 1,
+                  task: "Learn React",
+            },
 
-        {
-          id: 2,
-          task: 'Master React in 10 days'
-        }
-      ]
-    )
+            {
+                  id: 2,
+                  task: "Master React in 10 days",
+            },
+      ]);
+      const [isEditable, setIsEdiatable] = useState("");
 
-  return (
+      let nextId = new Date().getUTCMilliseconds();
 
+      function addnewtodo() {
+            if (todoinput === "") {
+                  alert("type something to add");
+            } else {
+                  updateTodo([
+                        ...todoList,
+                        {
+                              id: nextId,
+                              task: todoinput,
+                        },
+                  ]);
+                  updateinput("")
+            }
+      }
 
-    <div className="container mt-5 w-50">
-      <h3 className='text-center'>To-Do App using React</h3>
-      <div className="input-group">
-        <input className="form-control" type="text" />
-        <button button className="btn btn-primary">Add</button>
-      </div>
-      <ul className="list-group mt-4">
-        <li className="list-group-item">
-          <p>todo 1</p>
-          <button>❌</button>
-        </li>
-        <li className="list-group-item">
-          <p>todo 2</p>
-          <button>❌</button>
-        </li>
-      </ul>
-    </div>
+      console.log()
 
+      function deletetodo(id) {
+            let updatedtods = todoList.filter((todo) => {
+                  return todo.id != id;
+            });
+            updateTodo(updatedtods);
+      }
 
-  )
+      function updateTasks(id) {
+            setIsEdiatable(id);
+
+      }
+
+      const saveTodo = () => {
+            setIsEdiatable("");
+      }
+
+      return (
+            <div className="container mt-5 w-50">
+                  <h3 className="text-center">To-Do App using React</h3>
+                  <div className="input-group">
+                        <input className="form-control" type="text" value={todoinput} onChange={(e) => { let task = e.target.value; updateinput(task); }} />
+                        <button onClick={addnewtodo} className="btn btn-primary"> Add </button>
+                  </div>
+                  <ul className="list-group mt-4">
+                        {todoList.map((todo) => {
+                              return (
+                                    <li className="list-group-item" key={todo.id}>
+                                          <p contentEditable={isEditable === todo.id} style={{ border: isEditable === todo.id ? "2px solid black" : '' }} id={todo.id}>{todo.task}</p>
+                                          {isEditable === todo.id ? <button onClick={saveTodo}>🌎</button> : <button onClick={() => { updateTasks(todo.id) }} > 🥖</button >}
+                                          {isEditable === todo.id ? null : < button onClick={() => { deletetodo(todo.id) }}>❌</button>}
+                                    </li>
+                              );
+                        })}
+                  </ul>
+            </div>
+      );
 }
 
-export default App
+export default App;
